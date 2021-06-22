3fbf6f0808eec5409eba5e6da27612f372816f5d
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2021-06-22 17:38:41 +0500

    whyus page done

--- resources/views/frontend/pages/whySinotrans.blade.php
+++ resources/views/frontend/pages/whySinotrans.blade.php
@@ -25,7 +25,45 @@
 
     </section>
 
-    @component('frontend.components.whyus')
+    <section class="about about-2 about-5" id="about-5">
+        <div class="container">
+        <div class="row">
+        <div class="col-12 col-lg-5">
+        <div class="heading heading-9">
+        <p class="heading-subtitle">Providing Full Range Of Transportation Worldwide.</p>
+        <h2 class="heading-title">Why Sinotrans Limited?</h2>
+        </div>
+        <div class="about-block">
+        <div class="block-left">
+        <p>Sinotrans specializes in managing complex, high-value, heavy-lift and over-dimensional project cargo and international freight forwarding services.  Customers can count on us for reliable, professional, safe, and compliant cargo handling, while remaining responsive to constantly changing market needs.</p>
+        <p>The unbeatable combination of our technology, service and people for our diverse customer base has made us a leading integrated logistics service provider in Pakistan with a reputation for multi-modal project cargo success backed by proven innovative logistics solutions and service excellence.</p>
+        <p>In 7 short years, we have accomplished over 50 large projects, equalling over 1.5 million tons of cargo, thanks to our ever expanding team: Thanks to their extensive knowledge base and rich experience, unwavering adaptability as well as unrelenting commitment to success, we’re certain to achieve much more. We also equip them with state-of-the-art tools and equipment to help empower lasting business relationships and sustained success.</p>
+        <p>This also means we play a significant role in a diverse array of projects that contribute to the country’s national wealth, security, and environmental well-being: From wind, solar, and hydropower setups to equipment transport for the overall revamp of Pakistan’s national grid, shifting over 7500+ TEU’s, we proudly help deliver prosperity and progress</p>
+        </div>
+        </div>
+        <div class="signature-block">
+        <div class="signature-body">
+        <span>{{config('app.name')}}</span>
 
-    @endcomponent
+        </div><img style="width: 80px;" src="{{asset('/assets/images/logo/logo-dark.png')}}" alt="{{config('app.name')}}">
+        </div>
+        </div>
+
+        <div class="col-12 col-lg-6 offset-lg-1">
+        <div class="about-img abput-img-text">
+        <div class="about-img-warp">
+        <div class="bg-section"><img class="img-fluid" src="assets/images/about/2.jpg" alt="about Image"></div>
+        </div>
+        <div class="about-card">
+        <h6>Flexibile, Improved & Accelerated Solutions!</h6>
+        <p>Providing full range of service in the sphere transportation worldwide.</p><a href="javascript:void(0)"><i class="icon-arrow-right"></i> our borchure</a>
+        </div>
+        </div>
+        </div>
+        </div>
+
+        </div>
+
+        </section>
+
 @endsection
